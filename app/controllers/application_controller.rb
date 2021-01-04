552c075ce53d41ef51b1d70748b4156eec0fa175
Filipp Minayev <Filippmi@gmail.com>
2021-01-04 15:06:48 -0700

    added .env and .gitignore for session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,10 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :session
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :session_secret, ENV['session_secret']
   end
 
   get "/" do
