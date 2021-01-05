cd0b45da0abfbf2c8a8a5fcbd730594b8745e372
Filipp Minayev <Filippmi@gmail.com>
2021-01-05 14:43:32 -0700

    miss spell fix and added a home route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,13 +10,17 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
+    redirect '/home'
+  end
+
+  get "/home" do
     erb :welcome
   end
+
   helpers do
     def logged_in?
-      !!session[:user_id]
+      !!session[:current_user_id]
     end
   end
 
-  
 end
