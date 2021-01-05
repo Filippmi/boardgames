1704f6ed0f866b322b0317631edca3a129c882af
Filipp Minayev <Filippmi@gmail.com>
2021-01-05 14:44:13 -0700

    added logout route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,4 +13,9 @@ class SessionsController < ApplicationController
             redirect "/signup"
         end
     end
+
+    get '/logout' do
+        session.clear
+        redirect "/"
+    end
 end
