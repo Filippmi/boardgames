b751c3c9ce2f08a4a656e857612ab9ad5b1be0df
Filipp Minayev <Filippmi@gmail.com>
2021-01-05 15:15:58 -0700

    get and post route for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,22 @@ class SessionsController < ApplicationController
 
     get '/logout' do
         session.clear
-        redirect "/"
+        redirect "/home"
+    end
+
+    get '/login' do
+        
+        erb :'sessions/login'
+    end
+
+    post '/login' do
+        current_user = User.find_by_username(params[:user][:username]) # checks to see if current_user exists
+        if current_user && current_user.authenticate(params[:user][:password]) # checks to see if current_users password matches. "authenticats user"
+        #first makes sure the current_user exists, then it authenticats
+            session[:current_user_id] = current_user.id #logging in to sessions
+            redirect "/gg"
+        else
+            redirect "/login"
+        end
     end
 end
