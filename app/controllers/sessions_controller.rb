2b7605919b285e419ee85338a6a664e071b0fb5b
Filipp Minayev <Filippmi@gmail.com>
2021-01-05 14:30:42 -0700

    user signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,14 @@ class SessionsController < ApplicationController
 
         erb :'sessions/signup'
     end
+
+    post '/signup' do
+        current_user = User.new(params[:user])
+        if current_user.save
+            session[:current_user_id] = current_user.id #this is where the user is getting logged in
+            redirect "/gg"
+        else
+            redirect "/signup"
+        end
+    end
 end
