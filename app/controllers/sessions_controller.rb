a3fbd92913cb7ee32f8dc1638e7129dfafa17fbf
Filipp Minayev <Filippmi@gmail.com>
2021-01-06 11:15:36 -0700

    adding authorizations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
     get '/signup' do
-
+        redirect_if_logged_in
         erb :'sessions/signup'
     end
 
@@ -20,7 +20,7 @@ class SessionsController < ApplicationController
     end
 
     get '/login' do
-        
+        redirect_if_logged_in
         erb :'sessions/login'
     end
 
